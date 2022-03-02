39ae8f7e6fd53080735524147a21cc00b03b44df
Abel Lamas <abel.synyster@gmail.com>
2022-03-02 00:15:06 -0300

    fix error with urls

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from .views import IndexView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),    
-    path('', include('api.urls')),
+    path('', IndexView.as_view(), name='index'),
 ]
